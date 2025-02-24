7a7e706e2d466520f64bcbe25c54830af9978fe6
Jake Stewart <jake.stephen.stewart@gmail.com>
2025-02-24 21:21:38 +0800

    fix primitives

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-Object.prototype.$ = function(cb) {
+function objectPipe(cb) {
   return this instanceof Promise ? this.then(cb) : cb(this);
 }
+
+function primitivePipe(cb) {
+    return cb(this.valueOf());
+}
+
+Object.prototype.$ = objectPipe;
+String.prototype.$ = primitivePipe;
+Number.prototype.$ = primitivePipe;
+Boolean.prototype.$ = primitivePipe;
+Symbol.prototype.$ = primitivePipe;
 
 module.exports = {}
